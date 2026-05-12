738e1b020f0e4d359774042768f17b4445b5c8ab
AleDav73 <161150672+AleDav73@users.noreply.github.com>
2026-05-11 19:17:32 -0600

    Update __init__.py

--- main/models/__init__.py
+++ main/models/__init__.py
@@ -2,6 +2,7 @@ import torch
 from .rcnn import RCNN2d, predict_rcnn2d
 from .cgru import Conv2dGRU, predict_cgru
 from .rnn import RNN, predict_rnn
+from .cnn import CNN, predict_cnn
 
 
 def transpose_data(
@@ -10,17 +11,20 @@ def transpose_data(
 ) -> tuple[torch.Tensor, torch.Tensor]:
     """
     Returns inputs, labels
+    (# samples, seq_len, C_in, H, W) -> (seq_len, # samples, C_in, H, W)
     """
-    # from (# samples, seq_len, C_in, H, W) -> (seq_len, # samples, C_in, H, W)
     batch = batch.clone().transpose(0, 1)
     return batch[:split], batch[split:]
 
-    
+
 __all__ = [
     'RCNN2d',
     'predict_rcnn2d',
     'Conv2dGRU',
     'predict_cgru',
     'RNN',
-'predict_rnn',
+    'predict_rnn',
+    'CNN',
+    'predict_cnn',
+    'transpose_data',
 ]
